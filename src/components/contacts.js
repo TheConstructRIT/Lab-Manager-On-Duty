3b0760a7991355dbae2fb90f476c7bd3ae57dccd
aushe02 <ath9902@rit.edu>
2021-08-30 16:23:54 -0400

    upload files

---
+++ src/components/contacts.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { Container, Row, Col} from "shards-react";
+
+
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardImg,
+    CardBody,
+    CardFooter,
+    Button
+  } from "shards-react";
+
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "shards-ui/dist/css/shards.min.css"
+
+/*
+ * Unescapes an HTML string.
+ */
+function unescapeHTML(text) {
+    // Replace the characters.
+    text = text.replace("&amp;","&");
+    text = text.replace("&lt;","<");
+    text = text.replace("&lt;","<");
+
+    // Return the string.
+    return text;
+}
+
+/*
+ * Returns the link in the <a href="..."> text.
+ * Returns itself it if isn't used.
+ */
+function GetImageFromHREF(text) {
+    // Return the link if it is a link tag.
+    if (text != null) {
+        var start = text.indexOf(">");
+        if (start != -1) {
+            var subText = text.substring(start + 1);
+            var end = subText.indexOf("<");
+            if (end != -1) {
+                return unescapeHTML(subText.substring(0,end).trim());
+            }
+        }
+    }
+
+    // Return itself (not a link).
+    return unescapeHTML(text);
+}
+
+const Contacts = ({contacts}) => {
+    if (contacts.description == null || contacts.description.trim() == "") {
+        contacts.description = "constructlogo.png";
+    }
+
+    return (
+        
+    <div>
+    
+       
+        <Card>
+            <CardBody>
+            <CardImg top src={GetImageFromHREF(contacts.description)}/>
+            <center>
+                    <CardTitle>{contacts.summary}</CardTitle>
+            </center>
+            </CardBody>
+        </Card>
+       </div>
+        
+       
+          
+    )
+};
+
+export default Contacts
---
+++ src/components/noEmployees.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Container, Row, Col} from "shards-react";
+
+
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "shards-ui/dist/css/shards.min.css"
+  
+
+
+
+    return (
+        
+    <div>
+    
+            <center>
+                    <h1>There are no noE<h1>
+            </center>
+    
+       </div>
+        
+       
+          
+    )
+
+
+export default noEmployees
